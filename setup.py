1f4d9482006397cd0ead351092b5962b132d842c
FZZkill <changzhijie11@outlook.com>
2022-04-07 20:20:32 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 import setuptools #导入setuptools打包工具
  
-with open("README.md", "r", encoding="utf-8") as fh:
+with open("README.MD", "r", encoding="utf-8") as fh:
     long_description = fh.read()
  
 setuptools.setup(
-    #install_requires = ["rich>=12.0.1"],
     name="txvx", 
     version="1.0", 
     author="FZZkill",
@@ -12,12 +11,12 @@ setuptools.setup(
     description="更简单的配置",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    license="Mozilla Public License 2.0"
+    license="MPL",
     url="https://github.com/FZZkill/TxvxConfig",
     packages= setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Mozilla Public License 2.0",
+        "License :: OSI Approved :: MPL",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.8', 
